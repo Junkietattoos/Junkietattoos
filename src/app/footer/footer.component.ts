f88fdd4a36f6818ce0500b9ed8df1aa391f3407e
Lewis Stoll <Lewis.Stoll@deutschebahn.com>
2021-11-13 17:42:05 +0100

    fuck the google maps documentation （ミ￣ー￣ミ）

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+//TODO: Get the Customs Google Map 2 work u dumbass
+// this ur API KEY: AIzaSyAoM6xB1MUM50-5uN_0UIFhtYVDvW3EffE
+ //  map: google.maps.Map;
   constructor() { }
 
   ngOnInit(): void {
+ /** this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  }); **/
   }
-
 }
